ac341dcbb033ee786028625beb6f31e0f1283059
sergiobarria <sbarria.dev@gmail.com>
2022-10-21 21:07:02 -0500

    feat: add theme switcher

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			fontFamily: {
