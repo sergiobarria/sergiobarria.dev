a1bc094ad4c205bc034ce13012a2b8e061b0531d
sergiobarria <sbarria.dev@gmail.com>
2023-01-26 22:32:40 -0500

    feat: dashboard metrics

--- src/shared/components/svelte/TopTracks.svelte
+++ src/shared/components/svelte/TopTracks.svelte
@@ -1,18 +1,33 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import clsx from 'clsx';
-	import { useSWR } from 'sswr';
 
-	import type { TopSpotifyTracks, Track } from 'lib/spotify';
+	import type { TopSpotifyTracks } from 'lib/spotify';
+	import { BASE_URL, MODE } from '~/shared/constants';
+	import Loader from './Loader.svelte';
 
-	const { data } = useSWR<TopSpotifyTracks>('/api/spotify/top-tracks.json');
+	$: topTracks = getSpotifyTopTracks();
+
+	async function getSpotifyTopTracks() {
+		const res = await fetch(`${BASE_URL}/api/spotify/top-tracks.json`);
+		const data = await res.json();
+
+		return data as TopSpotifyTracks;
+	}
 </script>
 
-{#if !$data?.tracks}
-	<p class="mt-8 text-center opacity-70">No data available...</p>
-{:else}
+{#await topTracks}
+	<Loader />
+{:then data}
 	<ul class="mt-4 md:columns-2 gap-x-10">
-		{#each $data?.tracks as track, idx (track.id)}
-			<li class={clsx('flex items-center justify-between border-b border-surface-four')}>
+		{#each data?.tracks as track, idx (track.id)}
+			<li
+				class={clsx('flex items-center justify-between border-b border-surface-four')}
+				transition:fade={{
+					duration: 500,
+					delay: idx * 100,
+				}}
+			>
 				<div class="first-of-type:flex first-of-type:items-baseline">
 					<span class="text-sm font-medium text-font-two opacity-70">{idx + 1}</span>
 					<div class="flex flex-col p-5 pl-3">
@@ -38,4 +53,10 @@
 			</li>
 		{/each}
 	</ul>
-{/if}
+{:catch error}
+	{#if MODE === 'development'}
+		<pre class="mt-8 text-center opacity-70">{error.message}</pre>
+	{:else}
+		<p class="mt-8 text-center opacity-70">No data available...</p>
+	{/if}
+{/await}
