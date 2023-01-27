c0a4c352721f2c1cb3098b6e1df31686c756cb64
sergiobarria <sbarria.dev@gmail.com>
2023-01-26 23:16:14 -0500

    feat: add views counters

---
+++ src/shared/components/svelte/ViewsCounter.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { BASE_URL } from '~/shared/constants';
+
+	let views = 0;
+	export let slug = '';
+
+	onMount(async () => {
+		const res = await fetch(`${BASE_URL}/api/views/${slug}.json`, {
+			method: 'POST',
+		});
+		const data: { total: number } = await res.json();
+
+		views = data.total;
+	});
+</script>
+
+<span>{`${views} views` ?? '--'}</span>
