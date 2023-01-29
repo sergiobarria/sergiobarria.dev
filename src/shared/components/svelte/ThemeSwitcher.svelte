2b62df69660c2dfa8b5b08cfe2f17719ddb23dde
sergiobarria <sbarria.dev@gmail.com>
2023-01-29 17:26:27 -0500

    chore: update files, add icons

--- src/shared/components/svelte/ThemeSwitcher.svelte
+++ src/shared/components/svelte/ThemeSwitcher.svelte
@@ -1,73 +1,46 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import clsx from 'clsx';
-	import { Switch } from '@rgossiaux/svelte-headlessui';
+	import MoonIcon from '~/shared/icons/MoonIcon.svelte';
+	import SunIcon from '~/shared/icons/SunIcon.svelte';
 
-	let checked = false;
+	function toggleMode() {
+		const doc = document.documentElement;
+		const isDarkMode = doc.classList.contains('dark');
 
-	function toggleTheme(e: CustomEvent<boolean>) {
-		checked = e.detail;
-		const doc = document.firstElementChild;
-		const themePreference = checked ? 'dark' : 'light';
-
-		doc?.setAttribute('data-theme', themePreference);
-		localStorage.setItem('theme-preference', themePreference);
-	}
-
-	onMount(() => {
-		const themePreference = localStorage.getItem('theme-preference');
-		if (themePreference) {
-			checked = themePreference === 'dark';
+		if (isDarkMode) {
+			doc.classList.remove('dark');
+			window.localStorage.mode = 'light';
+		} else {
+			doc.classList.add('dark');
+			window.localStorage.mode = 'dark';
 		}
-	});
+	}
 </script>
 
-<Switch
-	{checked}
-	on:change={(e) => toggleTheme(e)}
+<button
+	type="button"
+	aria-label="Toggle dark mode"
 	class={clsx(
-		'relative inline-flex items-center rounded-full h-6 w-11',
-		checked ? 'bg-brand-accent' : 'bg-surface-four'
+		'px-3 py-2 transition rounded-full shadow-lg group bg-white/90 shadow-zinc-800/5',
+		'ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-teal-500/50'
 	)}
+	on:click={toggleMode}
 >
-	<span class="sr-only">toggle theme</span>
-	<span
-		class={clsx(
-			'flex items-center justify-center',
-			'inline-block w-4 h-4 transform rounded-full transition ease-in-out duration-200',
-			checked ? 'translate-x-6 bg-surface-four' : 'translate-x-1 bg-white'
+	<MoonIcon
+		styles={clsx(
+			'hidden h-6 w-6 fill-zinc-700 stroke-teal-500 transition dark:block',
+			'[@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-400',
+			'[@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10',
+			'[@media_not_(prefers-color-scheme:dark)]:stroke-teal-500'
 		)}
-	>
-		{#if !checked}
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-3 h-3 text-black"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-				/>
-			</svg>
-		{:else}
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-3 h-3 text-white"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-				/>
-			</svg>
-		{/if}
-	</span>
-</Switch>
+	/>
+	<SunIcon
+		styles={clsx(
+			'h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200',
+			' group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50',
+			'[@media(prefers-color-scheme:dark)]:stroke-teal-500',
+			'[@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50',
+			'[@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600'
+		)}
+	/>
+</button>
