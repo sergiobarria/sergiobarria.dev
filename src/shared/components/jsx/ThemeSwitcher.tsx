4d2093a736c829117661c3a6c4e35a64a55c9595
sergiobarria <sbarria.dev@gmail.com>
2023-04-09 12:30:39 -0500

    chore: change theme incons

--- src/shared/components/jsx/ThemeSwitcher.tsx
+++ src/shared/components/jsx/ThemeSwitcher.tsx
@@ -1,16 +1,20 @@
+import { useState } from 'react';
 import clsx from 'clsx';
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 
 export function ThemeSwitcher() {
-	const toggleMode = () => {
-		const doc = document.documentElement;
-		const isDarkMode = doc.classList.contains('dark');
+	const [isDarkMode, setIsDarkModel] = useState<boolean>(() =>
+		document?.documentElement?.classList?.contains('dark')
+	);
 
+	const toggleMode = () => {
 		if (isDarkMode) {
 			doc.classList.remove('dark');
+			setIsDarkModel(false);
 			window.localStorage.mode = 'light';
 		} else {
 			doc.classList.add('dark');
+			setIsDarkModel(true);
 			window.localStorage.mode = 'dark';
 		}
 	};
@@ -20,30 +24,26 @@ export function ThemeSwitcher() {
 			type="button"
 			aria-label="toggle dark mode"
 			className={clsx(
-				'px-3 py-2 transition rounded-full shadow-lg group bg-white/90 shadow-zinc-800/5',
-				'ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-teal-500/50'
+				'p-3 transition rounded-full shadow-lg group bg-white/90 shadow-zinc-800/5',
+				'ring-1 ring-zinc-900/5 backdrop-blur dark:bg-white dark:ring-white/10 dark:hover:ring-teal-500/50'
 			)}
 			onClick={toggleMode}
 		>
-			<MoonIcon
-				strokeWidth={0.5}
-				className={clsx(
-					'hidden h-6 w-6 fill-teal-700 stroke-teal-500 transition dark:block',
-					'[@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-400',
-					'[@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10',
-					'[@media_not_(prefers-color-scheme:dark)]:stroke-teal-500'
-				)}
-			/>
-			<SunIcon
-				strokeWidth={1}
-				className={clsx(
-					'h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200',
-					' group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50',
-					'[@media(prefers-color-scheme:dark)]:stroke-teal-500',
-					'[@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50',
-					'[@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600'
-				)}
-			/>
+			{isDarkMode ? (
+				<img
+					src="/images/moon-to-sun.svg"
+					alt="sun icon"
+					width={25}
+					className="transition duration-200"
+				/>
+			) : (
+				<img
+					src="/images/sun-to-moon.svg"
+					alt="moon icon"
+					width={25}
+					className="transition duration-200"
+				/>
+			)}
 		</button>
 	);
 }
