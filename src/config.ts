0c0efee305ec9b9a349b7a78448020e6d684e505
sergiobarria <sbarria.dev@gmail.com>
2023-08-11 23:49:06 -0500

    feat: main layout, home & about pages

---
+++ src/config.ts
@@ -0,0 +1,18 @@
+export default {
+    title: 'Home | Sergio Barria',
+    description: 'Engineer, developer, amateur writer',
+    socials: [
+        {
+            name: 'github',
+            url: 'https://github.com/sergiobarria'
+        },
+        {
+            name: 'linkedin',
+            url: 'https://www.linkedin.com/in/sergiobarria/'
+        },
+        {
+            name: 'email',
+            url: 'mailto:sbarria.dev@gmail.com'
+        }
+    ]
+}
