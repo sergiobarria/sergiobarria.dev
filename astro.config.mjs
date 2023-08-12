9f74d7bc07a45cc3706a0f4fd2eaaf448e08d6b0
sergiobarria <sbarria.dev@gmail.com>
2023-08-11 23:31:27 -0500

    chore: get assets, update configs

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config'
+
+import tailwind from '@astrojs/tailwind'
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://sergiobarria.dev',
+    integrations: [tailwind()],
+    experimental: {
+        assets: true
+    }
+})
