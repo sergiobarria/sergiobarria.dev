458b457359d4ddfcdc17fc65f4622ce6ca0ff62e
sergiobarria <sbarria.dev@gmail.com>
2023-08-15 16:37:06 -0500

    feat: add read progress bar

--- src/components/svelte/index.ts
+++ src/components/svelte/index.ts
@@ -1,3 +1,4 @@
 export { default as ViewsCounter } from './ViewsCounter.svelte'
 export { default as AnimatedBars } from './AnimatedBars.svelte'
 export { default as SpotifyCard } from './SpotifyCard.svelte'
+export { default as ProgressBar } from './ProgressBar.svelte'
