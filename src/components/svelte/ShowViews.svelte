501cc3f0e8e4fc606cc6f284dd0a1a0e48345c62
sergiobarria <sbarria.dev@gmail.com>
2023-08-30 19:34:05 -0500

    refactor: show views component

--- src/components/svelte/ShowViews.svelte
+++ src/components/svelte/ShowViews.svelte
@@ -1,28 +1,23 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-
     import type { DBPost } from '@/lib/schema';
 
     export let showTotal: boolean = false;
     export let slug: string = '';
-    let totalViews: number | undefined = 0;
 
-    onMount(async () => {
+    async function fetchViews() {
         const res = await fetch('/api/all-views.json');
         const data = (await res.json()) as DBPost[];
-        console.log('DATA: ', data);
 
         if (showTotal) {
-            totalViews = data.reduce((acc, curr) => acc + curr.views, 0);
+            return data.reduce((acc, curr) => acc + curr.views, 0);
         } else {
-            totalViews = data.find(post => post.slug === slug)?.views ?? 0;
+            return data.find(post => post.slug === slug)?.views ?? 0;
         }
-    });
-    console.log('totalViews: ', totalViews);
+    }
 </script>
 
-{#if totalViews}
-    <span>{totalViews}</span>
-{:else}
+{#await fetchViews()}
     <span>--</span>
-{/if}
+{:then totalViews}
+    <span>{totalViews}</span>
+{/await}
