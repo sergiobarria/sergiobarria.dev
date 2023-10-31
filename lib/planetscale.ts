4fbf3572aed0df4da0ace1c5c5c1e6d907d01801
sergiobarria <sbarria.dev@gmail.com>
2023-10-30 23:33:06 -0500

    fix: remove unstable cache

--- lib/planetscale.ts
+++ lib/planetscale.ts
@@ -1,7 +1,6 @@
 import 'server-only';
 
 import { connect, Config } from '@planetscale/database';
-import { unstable_cache } from 'next/cache';
 
 const config = {
     host: process.env.DATABASE_HOST,
@@ -17,24 +16,16 @@ type DBPost = {
     views: number;
 };
 
-export const getTotalViews = unstable_cache(
-    async () => {
-        const rows = (await conn.execute('SELECT id, views FROM posts'))?.rows as Omit<
-            DBPost,
-            'slug'
-        >[];
+export const getTotalViews = async () => {
+    const rows = (await conn.execute('SELECT id, views FROM posts'))?.rows as Omit<
+        DBPost,
+        'slug'
+    >[];
 
-        return rows.reduce((acc, { views }) => acc + views, 0);
-    },
-    ['posts-views-total'],
-    { revalidate: 1 }
-);
+    return rows.reduce((acc, { views }) => acc + views, 0);
+};
 
-export const getAllPostViews = unstable_cache(
-    async () => {
-        const result = await conn.execute('SELECT id, slug, views FROM posts');
-        return result?.rows as DBPost[];
-    },
-    ['single-post-views'],
-    { revalidate: 1 }
-);
+export const getAllPostViews = async () => {
+    const result = await conn.execute('SELECT id, slug, views FROM posts');
+    return result?.rows as DBPost[];
+};
