fd08f24484cb4b7dbf11ef450b62ff4822cc22c2
sergiobarria <sbarria.dev@gmail.com>
2024-01-10 21:56:05 -0500

    wip: start home page update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Geist', ...defaultTheme.fontFamily.sans]
+                sans: ['Inter', ...defaultTheme.fontFamily.sans]
             },
             typography: {
                 quoteless: {
